14e5d8400b1b67d4ca119142878853a230108838
vin <731315455@qq.com>
2019-03-28 17:10:45 +0800

    + f, interface autowired

---
+++ src/main/java/com/example/springboot/service/MyService.java
@@ -0,0 +1,15 @@
+package com.example.springboot.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by vin on 2019/3/19.
+ */
+@Service
+public class MyService {
+
+    public void doSomething() {
+
+        System.out.println("do something!!");
+    }
+}
