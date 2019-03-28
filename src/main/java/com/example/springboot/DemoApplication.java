8d823c6521b1074ab36f2b95d141bf38cbc47b01
vin <731315455@qq.com>
2019-03-28 17:33:51 +0800

    + f, logback , scan -> reload configuration ,  0. jmxConfigurator jconsole monitor , config vm options ,then use jconsole remote connect  -Dcom.sun.management.jmxremote.port=9779  -Dcom.sun.management.jmxremote.authenticate=false  -Dcom.sun.management.jmxremote.ssl=false  -Djava.rmi.server.hostname=127.0.0.1  1. STOUT, log file policy ,todo self filter on prod didn't out the console log  2. logback MDC , specified log by discriminator and test case

--- src/main/java/com/example/springboot/DemoApplication.java
+++ src/main/java/com/example/springboot/DemoApplication.java
@@ -1,12 +1,27 @@
 package com.example.springboot;
 
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+
+        log.info("start app");
+        SpringApplication.run(DemoApplication.class, args);
+
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("垃圾回收机制之前调用。。。");
+    }
+
 }
---
+++ src/main/java/com/example/springboot/controller/MdcController.java
@@ -0,0 +1,27 @@
+package com.example.springboot.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.log4j.MDC;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * Created by vin on 2019/3/22.
+ */
+
+@Slf4j
+@RestController
+public class MdcController {
+
+    @RequestMapping("mdc")
+    public void testMdc() {
+
+        MDC.put("Transaction-ID", UUID.randomUUID().toString());
+        log.info("mdc Hello world");
+        MDC.clear();
+        log.info(" Hello world");
+    }
+
+}
