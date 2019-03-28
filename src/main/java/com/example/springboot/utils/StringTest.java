ad3597f58b6fb59d386654d27e9640ba2acf3efd
vin <731315455@qq.com>
2019-03-28 17:07:01 +0800

    + f, string test, a++ ,++a ; jvm 入栈（药柜），b++ 药柜内执行，非 Rload

---
+++ src/main/java/com/example/springboot/utils/StringTest.java
@@ -0,0 +1,20 @@
+package com.example.springboot.utils;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+/**
+ * Created by vin on 2019/2/21.
+ */
+public class StringTest {
+
+
+    public static void main(String[] args) {
+
+        String string = "abc" + "efg";
+
+        String str1 = "aaa";
+        String str2 = str1 + "ccc";
+
+    }
+}
