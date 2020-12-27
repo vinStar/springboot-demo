5bbf508a70bfaccc3db238945117ccb8f838dc98
vin <vinton.liu@qq.com>
2020-12-27 22:55:00 +0800

    + f，1、 CompletableFuture

--- demo-boot/src/main/java/com/example/springboot/CompletableFuture/Shop.java
+++ demo-boot/src/main/java/com/example/springboot/CompletableFuture/Shop.java
@@ -1,9 +1,11 @@
 package com.example.springboot.CompletableFuture;
 
+import javax.annotation.processing.Completion;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
@@ -36,7 +38,7 @@ public class Shop {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         List<Shop> shops = Arrays.asList(new Shop("shop1"),
                 new Shop("shop2"),
                 new Shop("shop3"),
@@ -46,26 +48,33 @@ public class Shop {
                 new Shop("shop7"),
                 new Shop("shop8")
         );
-
+        //  method 1
         List<String> list = shops.parallelStream().map(shop ->
                 String.format("%s price is %.6f ", shop.getName(), shop.getPice("product")))
                 .collect(Collectors.toList());
 
         System.out.println(list);
 
-
+//
         Executor executor = Executors.newCachedThreadPool();
+        //  method 2==================
+//
+//        CompletableFuture<?>[] priceFuture = shops.parallelStream()
+//                .map(shop -> CompletableFuture
+//                        .supplyAsync(() -> shop.getPice("product"), executor)).toArray(size -> new CompletableFuture[size]);
+//
+//
+//        //allOf接收一个数组，当里面的CompletableFuture都完成的时候，就会执行下一个语句
+//        CompletableFuture.allOf(priceFuture).thenAccept((obj) -> System.out.println(" all done " + obj));
+//        //allOf接收一个数组，当里面的CompletableFuture有一个完成时，就会执行下一个语句
+//        //CompletableFuture.anyOf(priceFuture).thenAccept((obj) -> System.out.println("fastest anyOf done " + obj));
+//        System.out.println("aaa");
 
-        CompletableFuture<?>[] priceFuture = shops.parallelStream()
-                .map(shop -> CompletableFuture
-                        .supplyAsync(() -> shop.getPice("product"), executor)).toArray(size -> new CompletableFuture[size]);
 
+        //  method 3
+        CompletableFuture<Double> aa = CompletableFuture.supplyAsync(() -> new Shop("shop3").getPice("product"), executor);
+        System.out.println(" supplyAsync price " + aa.get());
 
-        //allOf接收一个数组，当里面的CompletableFuture都完成的时候，就会执行下一个语句
-          CompletableFuture.allOf(priceFuture).thenAccept((obj)->System.out.println(" all done "+obj));
-        //allOf接收一个数组，当里面的CompletableFuture有一个完成时，就会执行下一个语句
-        //CompletableFuture.anyOf(priceFuture).thenAccept((obj) -> System.out.println("fastest anyOf done " + obj));
-        System.out.println("aaa");
 
 
     }
